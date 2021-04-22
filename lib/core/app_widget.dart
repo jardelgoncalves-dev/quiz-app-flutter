eab10eba5ac0fa3fd60b7da42e6b7e47445fca0f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-21 23:17:45 -0300

    feat: add splash screen page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,29 +1,9 @@
+import 'package:quiz_app_flutter/screens/Splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Dev Quiz",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dev Quiz"),
-        ),
-        body: Center(
-            child: Container(
-          decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(10)),
-          child: Text(
-            "I'm a front-end developer",
-            style: TextStyle(color: Colors.white, fontSize: 30),
-          ),
-        )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
+    return MaterialApp(title: "DevQuiz", home: SplashPage());
   }
 }
---
+++ lib/screens/Splash/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/core/core.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(gradient: AppGradients.linear),
+        child: Center(child: Image.asset(AppImages.logo)),
+      ),
+    );
+  }
+}
