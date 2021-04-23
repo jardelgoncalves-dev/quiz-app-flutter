329e3cabf4c3cbdffc928231fdee2a4886e8b5ad
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-22 23:00:21 -0300

    feat: add app bar to home page (WIP)

---
+++ lib/screens/home/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/shared/widgets/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
