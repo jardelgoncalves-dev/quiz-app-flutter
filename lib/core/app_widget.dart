1283d7f8bce60586d63c2d090548e97ac74e7e4e
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2021-04-22 22:59:54 -0300

    chore: testing home page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,9 +1,9 @@
-import 'package:quiz_app_flutter/screens/Splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/screens/screens.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "DevQuiz", home: SplashPage());
+    return MaterialApp(title: "DevQuiz", home: HomePage());
   }
 }
